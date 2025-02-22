45b38ac3f68989785f8f498b7feabd05969aeaf9
Daniel3178 <Daniel.ibrahimi360@gmail.com>
2025-02-22 23:42:11 +0100

    Shadcn library is configured and a sample button component is used in home page

--- src/app/home/homeView.tsx
+++ src/app/home/homeView.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import {Button} from '@/components/shadcn/button'
 
 const HomeView = () => {
   return (
     <div className='h-[100px] bg-blue-500 text-white align-middle text-center'>
       <h1 className='text-2xl'>Home Page!!</h1>
+        <Button className='bg-blue-800 hover:bg-green-800  hover:duration-700' onClick={()=> alert("you clicked me!") }>Click me</Button>
     </div>
   )
 }
