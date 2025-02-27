5ae7baee71736fd08d50f16b51f6916ff692be67
Daniel3178 <Daniel.ibrahimi360@gmail.com>
2025-02-27 12:49:35 +0100

    Added some ui components and created a skeleton for responsive navbar component

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -4,7 +4,9 @@ import { StoreProvider } from "./store-provider";
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryProvider>
-      <StoreProvider>{children}</StoreProvider>
+      <StoreProvider>
+        {children}
+        </StoreProvider>
     </QueryProvider>
   );
 }
