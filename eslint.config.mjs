c0b6eb3ccc2d041397ce887a414d9674af01c148
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2025-02-27 14:30:17 +0100

    feat: setup new strict eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,196 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+import globals from "globals";
+import eslint from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import eslintImport from "eslint-plugin-import";
+import eslintComments from "eslint-plugin-eslint-comments";
+import prettier from "eslint-config-prettier";
+import compat from "eslint-plugin-compat";
+import alias from "eslint-import-resolver-alias";
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  eslint.configs.recommended,
+  {
+    ...prettier,
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2024,
+        React: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      import: eslintImport,
+      "eslint-comments": eslintComments,
+      compat: compat.configs["flat/recommended"],
+      alias,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+      "import/resolver": {
+        node: {
+          extensions: [".js", ".ts", ".tsx"],
+        },
+        alias: {
+          map: [["@", "./"]],
+          extensions: [".js", ".ts", ".tsx"],
+        },
+      },
+      // let the `compat` eslint plugin know what polyfills are installed, to remove false positives
+      polyfills: ["ResizeObserver"],
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/consistent-type-assertions": "error",
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-shadow": "error",
+      "@typescript-eslint/no-use-before-define": "warn",
+      "@typescript-eslint/no-useless-constructor": "error",
+      "@typescript-eslint/no-var-requires": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          varsIgnorePattern: "^_.*",
+          argsIgnorePattern: "^_.*",
+        },
+      ],
+      "@typescript-eslint/no-redeclare": "error",
+
+      "react/jsx-filename-extension": ["warn", { extensions: [".tsx"] }],
+      "react/jsx-key": "warn",
+      "react/default-props-match-prop-types": "off",
+      "react/forbid-prop-types": "off",
+      "react/no-unknown-property": ["error", { ignore: ["css"] }],
+      "react/prop-types": "off",
+      "react/require-default-props": "off",
+      "react/button-has-type": "warn",
+      "react/destructuring-assignment": "warn",
+      "react/function-component-definition": "warn",
+      "react/jsx-no-constructed-context-values": "warn",
+      "react/jsx-no-target-blank": "warn",
+      "react/jsx-no-useless-fragment": "warn",
+      "react/jsx-props-no-spreading": "warn",
+      "react/no-array-index-key": "warn",
+      "react/no-unstable-nested-components": "warn",
+
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+
+      "import/no-default-export": "error",
+      "import/extensions": [
+        "error",
+        "never",
+        {
+          styles: "always",
+          jpg: "always",
+          png: "always",
+          json: "always",
+        },
+      ],
+      "import/first": "error",
+      "import/newline-after-import": "error",
+      "import/no-absolute-path": "error",
+      "import/no-anonymous-default-export": "warn",
+      "import/no-dynamic-require": "error",
+      "import/namespace": ["error", { allowComputed: true }],
+      "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+      "import/no-import-module-exports": "error",
+      "import/no-mutable-exports": "error",
+      "import/no-self-import": "error",
+      "import/no-unresolved": "error",
+      "import/no-useless-path-segments": "error",
+      "import/order": [
+        "warn",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+        },
+      ],
+      "import/prefer-default-export": "off",
+
+      "eslint-comments/no-unused-disable": "error",
+      "eslint-comments/disable-enable-pair": [
+        "error",
+        { allowWholeFile: true },
+      ],
+
+      "no-shadow": "off",
+      "no-use-before-define": "off",
+      "no-useless-constructor": "off",
+      "no-redeclare": "off",
+      "no-restricted-exports": "off",
+      "no-unused-expressions": ["error", { allowTaggedTemplates: true }],
+      "no-unused-vars": "off",
+      "no-continue": "warn",
+      "no-param-reassign": "warn",
+      "no-plusplus": "warn",
+      "no-promise-executor-return": "warn",
+      "no-restricted-globals": "warn",
+      "no-restricted-properties": "warn",
+      "no-underscore-dangle": ["warn", { allow: ["__typename"] }],
+      "no-useless-escape": "warn",
+      "prefer-destructuring": "warn",
+      "prefer-template": "warn",
+      "prefer-const": "warn",
+    },
+  },
+  {
+    files: ["app/**/*.{ts,tsx}"],
+    rules: {
+      "import/no-default-export": "off",
+      "react-refresh/only-export-components": "off",
+    },
+  },
+  {
+    files: ["components/ui/**/*.{ts,tsx}"],
+    rules: {
+      "react/jsx-props-no-spreading": "off",
+      "react-refresh/only-export-components": "off",
+    },
+  },
+  {
+    files: ["components/auth/**/*.{ts,tsx}", "app/auth/**/*.{ts,tsx}"],
+    rules: {
+      "react/jsx-props-no-spreading": "off",
+    },
+  },
+  {
+    files: ["tailwind.config.ts"],
+    rules: {
+      "import/no-default-export": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-use-before-define": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
