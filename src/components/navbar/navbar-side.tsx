15969f721fc0e554ed8cc0e46f37009f8d4deca2
Daniel3178 <Daniel.ibrahimi360@gmail.com>
2025-03-28 20:51:18 +0100

    feat: develop interactive navbar for mobile/tables screen sizes

--- src/components/navbar/navbar-side.tsx
+++ src/components/navbar/navbar-side.tsx
@@ -23,84 +23,177 @@ import { Collapsible } from "@/components/ui/collapsible";
 
 
 export const SideNavbar = ()=> {
-  const [isOpen, setIsOpen] = React.useState(false);
+  const sections = [
+    {
+      title: "For Students",
+      items: [
+        {
+          label: "Job Board",
+          icon: "/navbar_icons/View.svg",
+          icon_tailwind: "h-[16px]",
+          links: [
+            { name: "Internship at Ericsson", url: "#" },
+            { name: "Internship at Spotify", url: "#" },
+            { name: "Explore More", url: "#" },
+          ],
+        },
+        {
+          label: "News",
+          icon: "/navbar_icons/File_dock.svg",
+          icon_tailwind: "text-white h-[16px]",
+          links: [
+            { name: "AI in the Workplace", url: "#" },
+            { name: "Future of Work", url: "#" },
+            { name: "Explore More", url: "#" },
+          ],
+        },
+        {
+          label: "Events",
+          icon: "/navbar_icons/Date_range_fill.svg",
+          icon_tailwind: "text-white h-[16px]",
+          links: [
+            { name: "AI in the Workplace", url: "#" },
+            { name: "Future of Work", url: "#" },
+            { name: "Explore More", url: "#" },
+          ],
+        },
+      ],
+    },
+    {
+      title: "For Companies",
+      items: [
+        {
+          label: "Become Partner",
+          icon: "/navbar_icons/User_add.svg",
+          icon_tailwind: " text-white h-[16px]",
+          links: [
+            { name: "AI in the Workplace", url: "#" },
+            { name: "Future of Work", url: "#" },
+            { name: "Explore More", url: "#" },
+          ],
+        },
+        {
+          label: "Announce Jobs",
+          icon: "/navbar_icons/Edit_alt.svg",
+          icon_tailwind: " text-white h-[16px]",
+          links: [
+            { name: "AI in the Workplace", url: "#" },
+            { name: "Future of Work", url: "#" },
+            { name: "Explore More", url: "#" },
+          ],
+        },
+        {
+          label: "Arrange Events",
+          icon: "/navbar_icons/Date_range_fill.svg",
+          icon_tailwind: " text-white h-[16px]",
+          links: [
+            { name: "AI in the Workplace", url: "#" },
+            { name: "Future of Work", url: "#" },
+            { name: "Explore More", url: "#" },
+          ],
+        }
+      ],
+    },
+    {
+      title: "About Us",
+      items: [
+        {
+          label: "Contact",
+          icon: "/navbar_icons/Message.svg",
+          icon_tailwind: " text-white h-[16px]",
+          links: [
+            { name: "AI in the Workplace", url: "#" },
+            { name: "Future of Work", url: "#" },
+            { name: "Explore More", url: "#" },
+          ],
+        },
+        {
+          label: "Team",
+          icon: "/navbar_icons/Group.svg",
+          icon_tailwind: " text-white h-[16px]",
+          links: [
+            { name: "AI in the Workplace", url: "#" },
+            { name: "Future of Work", url: "#" },
+            { name: "Explore More", url: "#" },
+            { name: "Explore More", url: "#" },
+            { name: "Explore More", url: "#" },
+            { name: "Explore More", url: "#" },
+            { name: "Explore More", url: "#" },
+            { name: "Explore More", url: "#" },
+          ],
+        },
+        {
+          label: "Speakers",
+          icon: "/navbar_icons/Group_scan.svg",
+          icon_tailwind: " text-white h-[16px]",
+          links: [
+            { name: "AI in the Workplace", url: "#" },
+            { name: "Future of Work", url: "#" },
+            { name: "Explore More", url: "#" },
+          ],
+        }
+      ],
+    },
+  ];
+
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <img src="/navbar_icons/Menu.svg" className="h-[40px] md:h-[60px] md:w-[60px] w-[40px]"/>
-        {/* <Button variant="outline">Open</Button> */}
+        <img src="/navbar_icons/Menu.svg" className="h-[40px] md:h-[60px] md:w-[60px] w-[40px]" />
       </SheetTrigger>
-      <SheetContent className="bg-[#1751A6] text-white border border-[#1751A6]">
+      <SheetContent className="bg-[#1751A6] text-white border border-[#1751A6]  flex flex-col max-h-screen overflow-y-auto">
         <SheetHeader>
-          <SheetTitle>
-          <img src="/navbar_icons/logo.svg" className="h-[40px] w-[100px] top-0"/>
+          <SheetTitle className="cursor-pointer">
+            <img src="/navbar_icons/logo.svg" className="h-[40px] w-[100px] top-0" />
           </SheetTitle>
-          <SheetDescription>
-          </SheetDescription>
-          
         </SheetHeader>
 
-        <Accordion type="single" collapsible>
-          <AccordionItem value="item-1">
-            <AccordionTrigger className="mt-10 px-4 h-8 rounded-lg">
-              {/* <Button variant="outline" className="w-full">Learn</Button> */}
-              <Label>Learn</Label>
-            </AccordionTrigger>
-            <AccordionContent>
-              <div className="flex  ml-[30px]">
-                <div className="border"></div>
-                <ul className="flex-col   w-[400px] px-1 md:w-[400px] lg:w-[500px] ">
-                  <li className=" h-5 rounded-md px-2">
-                    <Label className="">
-                      Getting Started with AI
-                    </Label>
-                  </li>
-                  <li className=" h-5 rounded-md px-2 group cursor-pointer">
-                    <Label className="cursor-pointer  group-hover:underline transition-all duration-300">
-                      KTH Courses About AI
-                    </Label>
-                  </li>
-                  <li
-                    className=" h-5 rounded-md px-2 overflow-hidden whitespace-nowrap text-ellipsis"
-                    title="Tutorial Challenge: Decision Trees and Forests and some other stuff"
-                  >
-                    <Label className="">
-                      Tutorial Challenge: Decision Trees and Forests and some other stuff
-                    </Label>
-                  </li>
-                </ul>
+        {sections.map((section, index) => (
+          <div key={index} className="mt-4">
+            <Label className="text-sm font-semibold text-[#C4C6CD]">{section.title}</Label>
+            {section.title === "For Students" && (
+              <div className="flex flex-col  mt-2">
+                <div className="flex items-center mb-2 ml-1 group cursor-pointer justify-start space-x-4 w-full">
+                <img src="/navbar_icons/User.svg" className=" ml-1 h-[18px]" />
+                <Label className="text-sm font-sans cursor-pointer group-hover:underline transition-all duration-300 ">Log in </Label>
+                  </div>
+                <hr></hr>
               </div>
-            </AccordionContent>
-          </AccordionItem>
+            )}
+            <Accordion type="single" collapsible className="text-sm pt-1 pb-2">
+              {section.items.map((item, itemIndex) => (
+                <AccordionItem key={itemIndex} value={`item-${index}-${itemIndex}`}> 
+                  <AccordionTrigger className=" h-8 rounded-lg">
+                    <div className="flex items-center justify-start space-x-2 w-full">
+                      <div className="p-2 rounded">
+                        <img src={item.icon} className={`${item.icon_tailwind}`} />
+                      </div>
+                      <Label className="font-sans cursor-pointer text-sm">{item.label}</Label>
+                    </div>
+                  </AccordionTrigger>
+                  <AccordionContent>
+                  <div className="flex pb-2 pt-1 ml-[15px]">
+                  <div className="border"></div>
+                    <ul className="flex-col w-[400px] px-1 md:w-[400px] lg:w-[500px]">
+                      {item.links.map((link, linkIndex) => (
+                        <li key={linkIndex} className="h-5 rounded-md px-2 flex items-center text-sm group cursor-pointer">
+                          <Label className="cursor-pointer font-sans font-light group-hover:underline transition-all duration-300">
+                            {link.name}
+                          </Label>
+                          <img src="/navbar_icons/External.svg" className="invert ml-1 h-[18px]" />
+                        </li>
+                      ))}
+                    </ul>
+                    </div>
 
-          <AccordionItem value="item-2">
-            <AccordionTrigger className=" px-4 h-8  rounded-lg bg-slate-background">
-              {/* <Button variant="outline" className="w-full">Learn</Button> */}
-              <Label>About</Label>
-            </AccordionTrigger>
-            <AccordionContent>
-              <div className="flex  ml-[30px]">
-                <div className="border  "></div>
-                <ul className="flex-col   w-[400px] px-1 md:w-[400px] lg:w-[500px] ">
-                  <li className=" h-5 rounded-md px-2">
-                    <Label className="">Contact</Label>
-                  </li>
-                  <li className="h-5 rounded-md px-2">
-                    <Label className="">Team</Label>
-                  </li>
-                </ul>
-              </div>
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
+                  </AccordionContent>
+                </AccordionItem>
+              ))}
+            </Accordion>
+          </div>
+        ))}
 
-        {/* <SheetHeader className="mt-10">
-          <SheetTitle>Admin Dashboard</SheetTitle>
-          <SheetDescription>
-          </SheetDescription>
-        </SheetHeader> */}
-
-        <SheetFooter className="mt-[600px] absolute">
+        <SheetFooter className=" relative self-center pt-24">
           <SheetClose asChild>
             <Label>Developed by IT-Team</Label>
           </SheetClose>
