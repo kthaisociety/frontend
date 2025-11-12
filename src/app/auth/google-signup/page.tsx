f7e041ee706b93a53b15fec0b86f28f8461c66e9
Daniel3178 <Daniel.ibrahimi360@gmail.com>
2025-11-12 17:14:46 +0100

    init: add google library dependency for google auth

---
+++ src/app/auth/google-signup/page.tsx
@@ -0,0 +1,5 @@
+import GoogleSignup from "@/components/google-auth/google-signup-button";
+
+export default function GoogleSignupPage() {
+  return <GoogleSignup />;
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/components/providers";
 import { Navbar } from "@/components/navbar/navbar";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <Script
+          src="https://accounts.google.com/gsi/client"
+          strategy="beforeInteractive" 
+        />
         <Providers>
           <Navbar />
           {children}
