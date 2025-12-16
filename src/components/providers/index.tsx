9e912e37f2c9e84ea3e97da1291c3710ffb95fb4
Daniel3178 <Daniel.ibrahimi360@gmail.com>
2025-12-16 15:56:52 +0100

    feat: configure redux and authentication provider (google OAuth) within React Root (wrappers)

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,5 +1,21 @@
+"use client";
+import { AppStore, makeStore } from "@/lib/model/store";
+import { AuthInitializer } from "./auth-initializer";
 import { QueryProvider } from "./query-provider";
+import { Provider } from "react-redux";
+import { useRef } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <QueryProvider>{children}</QueryProvider>;
+  const storeRef = useRef<AppStore>(null);
+  if (!storeRef.current) {
+    storeRef.current = makeStore();
+  }
+
+  return (
+    <Provider store={storeRef.current}>
+      <QueryProvider>
+        <AuthInitializer>{children}</AuthInitializer>
+      </QueryProvider>
+    </Provider>
+  );
 }
