8ce8853aed13cab475792a13018cea5cd2bba061
Daniel3178 <Daniel.ibrahimi360@gmail.com>
2025-12-16 15:55:30 +0100

    feat: create api with RTK library for authentication and create a slice to manage the state of the application with regard to authentication

---
+++ src/lib/model/apis/internal-apis.ts
@@ -0,0 +1,51 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { clearUser } from "../slices/auth-slice/authSlice";
+export const internalApi = createApi({
+  reducerPath: "internalApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "/", credentials: "include" }),
+  endpoints: (builder) => ({
+    getMe: builder.query({
+      query: () => "api/auth/me",
+    }),
+    getProfile: builder.query({
+      query: () => "api/auth/profile",
+    }),
+    getMeMocked: builder.query({
+      query: () => "testServerApi",
+    }),
+    googleLogin: builder.mutation({
+      query: (code: string) => ({
+        url: "testServerApi/auth",
+        method: "POST",
+        body: { code },
+        headers: {
+          Authorization: `Bearer ${code}`,
+        },
+      }),
+    }),
+    // testing with testServerApi
+    logout: builder.mutation({
+      query: () => ({
+        url: "testServerApi/logout",
+        method: "POST",
+      }),
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+        try {
+          await queryFulfilled;
+          dispatch(clearUser());
+          dispatch(internalApi.util.resetApiState());
+        } catch (err) {
+          console.log("Logout error:", err);
+        }
+      },
+    }),
+  }),
+});
+
+export const {
+  useGetMeQuery,
+  useGetMeMockedQuery,
+  useGetProfileQuery,
+  useGoogleLoginMutation,
+  useLogoutMutation,
+} = internalApi;
---
+++ src/lib/model/slices/auth-slice/authSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface User {
+  name: string;
+  email: string;
+  picture: string;
+  role: string;
+}
+
+interface AuthState {
+  user: User | null;
+  isLoggedIn: boolean;
+  loading: boolean;
+  error?: string;
+}
+
+const initialState: AuthState = {
+  user: null,
+  isLoggedIn: false,
+  loading: true, // start as loading until we check session
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<User>) => {
+      state.user = action.payload;
+      state.isLoggedIn = true;
+      state.loading = false;
+    },
+    clearUser: (state) => {
+      state.user = null;
+      state.isLoggedIn = false;
+      state.loading = false;
+    },
+    setLoading: (state, action: PayloadAction<boolean>) => {
+      state.loading = action.payload;
+    },
+  },
+});
+
+export const { setUser, clearUser, setLoading } = authSlice.actions;
+export default authSlice.reducer;
